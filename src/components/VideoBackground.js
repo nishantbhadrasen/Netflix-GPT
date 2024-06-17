c5671a9eae14f4ae07f49a0c41bda75e69e67fef
Nishant <de.nishantbhadrasen@gmail.com>
2024-06-17 14:33:40 +0200

    refactor: Add useMovieTrailer refactored hook to the VideoBackground

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,32 +1,17 @@
-import { API_OPTIONS } from "../utils/constants";
-import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import useMovieTrailer from "../hooks/useMovieTrailer";
 
 const VideoBackground = ({ movieId }) => {
-  //fetch trailer video
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
 
-  const getMovieVideos = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/653346/videos?language=en-US",
-      API_OPTIONS
-    );
-    const json = await data.json();
-    //console.log(json);
-
-    const filterData = json.results.filter((video) => video.type === "Trailer");
-    const trailer = filterData.length ? filterData[0] : json.results[0];
-    console.log(trailer);
-  };
-
-  useEffect(() => {
-    getMovieVideos();
-  }, []);
+  useMovieTrailer(movieId);
 
   return (
     <div>
       <iframe
         width="560"
         height="315"
-        src={"https://www.youtube.com/embed/" + trailer.key}
+        src={"https://www.youtube.com/embed/" + trailerVideo?.key}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
