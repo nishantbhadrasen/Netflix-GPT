fb2aded018c46ec1b121ac678b1207edcba10cfa
Nishant <de.nishantbhadrasen@gmail.com>
2024-06-17 07:44:14 +0200

    feat: Add to do's in the browse

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "./Header";
 
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 
 const Browse = () => {
   useNowPlayingMovies();
@@ -9,6 +11,16 @@ const Browse = () => {
   return (
     <div>
       <Header />
+      <MainContainer />
+      <SecondaryContainer />
+      {/*
+          Main Container
+            - VideoBackground
+            - VideoTitle
+          SecondaryContainer 
+            - MovieList * n
+            - cards * n
+      */}
     </div>
   );
 };
