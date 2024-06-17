a86b202673a40d46a913cc392140344c87a2c9ec
Nishant <de.nishantbhadrasen@gmail.com>
2024-06-17 20:51:09 +0200

    feat: Add util file for multiple languages

---
+++ src/utils/languageConstants.js
@@ -0,0 +1,16 @@
+const lang = {
+  en: {
+    search: "Search",
+    gptSearchPlaceHolder: "What would you like to watch today?",
+  },
+  hindi: {
+    search: "खोज",
+    gptSearchPlaceHolder: "आज आप क्या देखना चाहेंगे?",
+  },
+  de: {
+    search: "Suchen",
+    gptSearchPlaceHolder: "Was möchtest du heute sehen?",
+  },
+};
+
+export default lang;
