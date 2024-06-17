2356507ed05b721b468bbede935354f75a5a05aa
Nishant <de.nishantbhadrasen@gmail.com>
2024-06-17 15:20:09 +0200

    feat: Make MainContainer Visually appealing doing Tailwind CSS

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -7,14 +7,19 @@ const VideoBackground = ({ movieId }) => {
   useMovieTrailer(movieId);
 
   return (
-    <div>
-      <iframe
-        width="560"
-        height="315"
-        src={"https://www.youtube.com/embed/" + trailerVideo?.key}
-        title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      ></iframe>
+    <div className="w-screen h-screen">
+      <div className="relative w-full h-full">
+        <iframe
+          className="absolute top-0 left-0 w-full h-full aspect-video"
+          src={
+            "https://www.youtube.com/embed/" +
+            trailerVideo?.key +
+            "?&autoplay=1&mute=1"
+          }
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        ></iframe>
+      </div>
     </div>
   );
 };
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,13 +1,13 @@
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="pt-36 px-12">
+    <div className="w-screen h-full pt-[20%] px-24 absolute text-white z-20 bg-gradient-to-r from-black via-transparent to-transparent">
       <h1 className="text-6xl font-bold">{title}</h1>
       <p className="py-6 text-lg w-1/4">{overview}</p>
-      <div className="">
-        <button className="bg-gray-500 text-white  p-4 px-12 text-lg bg-opacity-50 rounded-lg">
+      <div>
+        <button className="bg-white text-black p-4 px-12 text-lg  rounded-lg hover:bg-opacity-80">
           ▶️ Play
         </button>
-        <button className="bg-gray-500 mx-2 text-white  p-4 px-12 text-lg bg-opacity-50 rounded-lg">
+        <button className="bg-gray-500 mx-2 text-white p-4 px-12 text-lg bg-opacity-50 rounded-lg hover:bg-opacity-80">
           More Info
         </button>
       </div>
