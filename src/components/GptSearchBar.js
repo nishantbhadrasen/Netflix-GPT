0ee40e2a36b6f0550585d0622efaa2df72001c90
Nishant <de.nishantbhadrasen@gmail.com>
2024-06-18 10:53:23 +0200

    feat: Add GptSearchBar component using useGptMovieSearch custom hook

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,20 +1,50 @@
+import { useRef } from "react";
 import { useSelector } from "react-redux";
 import lang from "../utils/languageConstants";
+import useGptMovieSearch from "../hooks/useGptMovieSearch";
 
 const GptSearchBar = () => {
   const langKey = useSelector((store) => store.config.lang);
+  const searchText = useRef(null);
+  const { searchGptMovies, loading, error } = useGptMovieSearch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleGptSearchClick = async () => {
+    try {
+      const query = searchText.current.value;
+      //console.log("Search Query: ", query);
+      await searchGptMovies(query);
+    } catch (err) {
+      //console.error("Error during GPT Movie Search: ", err.message);
+    }
+  };
+
   return (
-    <div className="pt-[10%] flex flex justify-center">
-      <form className=" w-1/2 bg-black grid grid-cols-12">
+    <div className="pt-[10%] flex justify-center">
+      <form
+        className="w-1/2 bg-black grid grid-cols-12"
+        onSubmit={handleSubmit}
+      >
         <input
+          ref={searchText}
           type="text"
-          className=" p-4 m-4 col-span-9"
+          className="p-4 m-4 col-span-9"
           placeholder={lang[langKey].gptSearchPlaceHolder}
-        ></input>
-        <button className="col-span-3 m-4 py-2 px-4 bg-red-700 text-white rounded-lg ">
-          {lang[langKey].search}
+          disabled={loading}
+        />
+        <button
+          type="button"
+          className="col-span-3 m-4 py-2 px-4 bg-red-700 text-white rounded-lg"
+          onClick={handleGptSearchClick}
+          disabled={loading}
+        >
+          {loading ? "Loading..." : lang[langKey].search}
         </button>
       </form>
+      {error && <div className="text-red-500 mt-2">{error}</div>}
     </div>
   );
 };
