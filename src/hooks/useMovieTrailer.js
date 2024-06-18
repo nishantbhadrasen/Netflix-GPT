13f6045fcaf0ee41dd55a1602b8aadf590a08464
Nishant <de.nishantbhadrasen@gmail.com>
2024-06-18 11:46:29 +0200

    feat: Add useSelector to multiple hooks to retrieve data from Redux store and conditionalized API calls based on existing data. (Memoization)

--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -1,10 +1,12 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constants";
 import { addTrailerVideo } from "../utils/moviesSlice";
 import { useEffect } from "react";
 
 const useMovieTrailer = (movieId) => {
   const dispatch = useDispatch();
+
+  const trailerVideo = useSelector((store) => store.movies.trailerVideo);
 
   const getMovieVideos = async () => {
     const data = await fetch(
@@ -14,17 +16,16 @@ const useMovieTrailer = (movieId) => {
       API_OPTIONS
     );
     const json = await data.json();
-   
 
     const filterData = json.results.filter((video) => video.type === "Trailer");
-   
+
     const trailer = filterData.length ? filterData[0] : json.results[0];
-   
+
     dispatch(addTrailerVideo(trailer));
   };
 
   useEffect(() => {
-    getMovieVideos();
+    !trailerVideo && getMovieVideos();
   }, []);
 };
 
